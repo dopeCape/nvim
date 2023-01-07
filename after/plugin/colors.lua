eecaf1b78740020e35bd5e095c553612b053f916
tejes <tmank14319@gmail.com>
2023-01-07 18:58:31 +0530

    copied from primeagen

---
+++ after/plugin/colors.lua
@@ -0,0 +1,8 @@
+function ColorMyPencils(color)
+	vim.o.background ="dark"
+	color = color or "gruvbox"
+	vim.cmd.colorscheme(color)
+
+
+end
+ColorMyPencils()
---
+++ lua/tejas/init.lua
@@ -0,0 +1,2 @@
+require("tejas.remap")
+print ("hello")
